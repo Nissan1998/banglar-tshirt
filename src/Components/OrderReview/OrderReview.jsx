61a7fdf4de7205968f7ab6ee5b81fb27f23aadc5
Nissan1998 <jnnissanm@gmail.com>
2023-04-08 01:25:57 +0600

    dataLoaded

---
+++ src/Components/OrderReview/OrderReview.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const OrderReview = () => {
+  return (
+    <div>
+      <h3>Order Review</h3>
+    </div>
+  );
+};
+
+export default OrderReview;
+<h3>Order Review</h3>;
